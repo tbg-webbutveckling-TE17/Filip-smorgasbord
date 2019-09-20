1220dfdb293d4a0802b92b8fd0cb1c2f11b2036e
Filip Gustafsson <filigust@lycksele.se>
2019-09-20 11:21:45 +0200

    jss

--- projects/webbutveckling 2/javascript grunder/assets/js/test.js
+++ projects/webbutveckling 2/javascript grunder/assets/js/test.js
@@ -1,1 +1,63 @@
-alert ('are we connected');
+//alert ('are we connected') pop up
+
+var myName = "Filip"; // datatyp: string
+var myAge = 18; //datatyp: number
+var isEvil = false; //datatyp: boolean
+
+// single line comment
+/*
+      block comment
+*/
+
+/*
+
+         if/else
+*/
+
+if(myAge < 18) {
+   //alert('you can not take a drivers license')
+} else {
+   //alert('congrats')
+}
+
+if (myName == 'Filip') {
+    //alert('welcome,' + myName);
+} else {
+    //alert('you are not welcome')
+}
+
+if (myAge == '32'){ // == equals to, === equals to and same datatyp
+   // alert('welcome')
+} else {
+   // alert('you are not welcome')
+}
+
+var hasWatched = false; // add to watchlist
+
+if(isEvil == true) {
+   // alert('du är ond')
+} else {
+    //alert('du är god')
+}
+
+/* 
+****************************
+   guessing app
+***************************
+*/
+
+var secretNumber = 7;
+var userinput = Number(prompt('guess a number between 0 - 10'));
+
+if(userinput == secretNumber) {
+     document.writeln('grattis! du har gissat rätt <br>');
+} else if() {
+
+} else if() {
+
+} else {
+
+}
+// if userinput === 6 or 8 'det bräns
+// if userinput is less than secretNumber "du gissar för lågt"
+// if userinput is greater than secretNumber "du gissade för högt"
