38d2a3758eeb3e641ab69bcf3a36fe7e30453a1e
Filip Gustafsson <filigust@lycksele.se>
2019-09-27 11:20:40 +0200

    For

---
+++ projects/webbutveckling 2/javascript grunder/assets/js/3-for.js
@@ -0,0 +1,14 @@
+for(var i = 0; i < 6; i++) {
+    console.log(i);
+}
+
+var str = 'hello world';
+for(var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
+
+var myArr = ["HTML", "CSS", "JS", 42, TRUE];
+var len = myArr.length;
+for (var i = 0; i < len; i++) {
+    console.log(myArr[i]);
+}
